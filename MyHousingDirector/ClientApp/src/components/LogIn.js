2935202a84d0fddadf154c85546b5abe0e47c13c
Nicholas Gaston <nicholas.gaston@snhu.edu>
2021-09-21 10:06:07 -0400

    Lightley styled the login-page

--- MyHousingDirector/ClientApp/src/components/LogIn.js
+++ MyHousingDirector/ClientApp/src/components/LogIn.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import './LogIn.css'
 
 
 export class LogIn extends Component {
@@ -7,8 +8,20 @@ export class LogIn extends Component {
     render() {
 
         return (
-            <div>
-
+            <div >
+                <form className= 'login-box' >
+                    <label className = 'label '>
+                        Email:
+                        
+                    </label>
+                    <input className='input-box' type="text" name="email" />
+                    <label className='label'>
+                        Password:
+                       
+                    </label>
+                    <input className='input-box' type="password" name="password" />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
             );
     }
