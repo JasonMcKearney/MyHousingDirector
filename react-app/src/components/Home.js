cbee6258c298e2222780ba39b48e9992f63fb68e
nathanielpatrick.lemana <nathanielpatrick.lemana@snhu.edu>
2021-09-30 04:07:14 -0400

    Attempted to make Jack's latest JS files work with the new setup, not sure if it's working

---
+++ react-app/src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export class Home extends Component {
+
+    state = { buttonState: false};
+
+    render() {
+        return (
+            <div>
+               
+                <h1 className = 'home-header'>Welcome to MyHousingDirector</h1>
+                <br />
+                <LinkContainer to={'/LogIn'}>
+                    <button className='btn-login' > LogIn </button>
+                </LinkContainer>
+           </div>
+        );
+    }
+}
+
+
+
+
+
+    
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import 'antd/dist/antd.css';
+import React from 'react';
+import { Route } from 'react-router';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { LogIn } from './components/LogIn';
+import { Home } from './components/Home';
+import registerServiceWorker from './registerServiceWorker';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter basename={baseUrl}>
+    <div style={{
+      height: '100%'
+    }}>
+      <Route path='/' exact component={LogIn}  />
+      <Route path='/LogIn' component={LogIn}  />  
+      <Route path='/home' component={Home}  />  
+      
+      {/* <App /> */}
+    </div>
+  </BrowserRouter>,
+  rootElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
