877ffe661cf2e1198f72c8e9b1897fe1b547ff70
NicholasGaston <59942769+NicholasGaston@users.noreply.github.com>
2021-10-19 23:09:49 -0400

    Created search bar and Search css file

--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 
+import {
+    Form,
+    Input,
+    Select,
+    Checkbox,
+    Button,
+} from 'antd';
 
 export default class Search extends Component {
 
@@ -9,10 +16,24 @@ export default class Search extends Component {
         return (
             <div>
                 <h1>Search</h1>
+            
+            <div>
+            
+            <Form>
+            
+            <Form.Item
+                id = "searchBar"
+                name="searchBar"
+                label="Search"
+            >
+
+            <Input />
+
+            </Form.Item>
+            
+            </Form>
             </div>
-
-
-
+        </div>
         );
     }
 
