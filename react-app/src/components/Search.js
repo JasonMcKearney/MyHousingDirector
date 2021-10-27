7ff9ba6d6723bf3bc69e7fbcbab9e185518661e6
NicholasGaston <59942769+NicholasGaston@users.noreply.github.com>
2021-10-26 20:35:34 -0400

    Added a result for backend team

--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -16,14 +16,12 @@ export default class Search extends Component {
                             <div >
 
                                 <div id = "wrapper">
-                                    <div ClassName="search-bar">
+                                    <div >
                                      <input id = "searchBar" Placeholder="Please input a Students name or ID" />
                                      </div>
                                    
-                                    <div ClassName = "search-button">
-                                        <button htmlType="submit" >
-                                            Search
-                                        </button>
+                                    <div >
+                                        <button id = "primary-button" ClassName="primary-button" htmlType="submit" >Search </button>
                                     </div>
                                 </div>
 
@@ -39,19 +37,9 @@ export default class Search extends Component {
                 </div>
 
                 <div className="resultsBox">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    <div className ="result-node"> 
+                        <a className="student-name">Jason McKearney</a>
+                    </div>
 
                 </div>
             </div>
