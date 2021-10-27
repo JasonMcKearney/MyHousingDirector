6a7ed5bb21327cc7309f055fa2c960d87af8f3d1
jackwulanbatu <60981213+jackwulanbatu@users.noreply.github.com>
2021-10-28 06:22:27 +0800

    Student page visual finished

--- react-app/src/components/Student.js
+++ react-app/src/components/Student.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import {
-    Layout, Menu, Breadcrumb, Button, Form} from 'antd';
+    Layout, Menu, Breadcrumb, Button, Form
+} from 'antd';
 import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
@@ -11,61 +12,68 @@ const { Header, Content, Footer, Sider } = Layout;
 
 export class Student extends Component {
 
-  render() {
+    render() {
 
-    return (
-        <Layout style={{ height: '100%' }}>
-                <Menu className="Student-nav-bar" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                    <Link to="/student/home">home</Link>
-                    </Menu.Item>
-                <Menu.Item key="2">
-                    <Link to="/student/nav1">nav1</Link>
-                    </Menu.Item>
-                <Menu.Item key="3">
-                    <Link to="/student/nav2">nav2</Link>
-                    </Menu.Item>
+        return (
+            <Layout style={{ height: '100%' }}>
+                <div style={{
+                    background:'#47F84E',
+                    display: 'flex',
+                    alignItems: 'center'
 
+                }}>
+                    <Menu style={{
+                        flex: 1
+                    }} className="Student-nav-bar" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to="/student/home">home</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="/student/nav1">nav1</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Link to="/student/nav2">nav2</Link>
+                        </Menu.Item>
 
-
-                    <Form.Item Logout={{ offset: 8, span: 16 }}>
+                    </Menu>
+                    <div>
                         <Button onClick={() => {
                             this.props.history.push('/LogIn');
                         }} type="primary" htmlType="Logout">
                             Logout
-                            </Button>
+                        </Button>
 
-                    </Form.Item>
-
-                </Menu>
-
-
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '40px 0' }}>
-          </Breadcrumb>
-                <Layout className="Student-page-background" style={{ height: '100%' }}>
-                    <Content style={{ margin: '24px 16px 0' }}>
-                        <div className="Student-content-background" style={{ padding: 24, height: '100%' }}>
-                            <Switch>
-
-                                <Route path='/student/Home' render={() => { return (<h1>Home</h1>) }} />
-                                <Route path='/student/nav1' render={() => { return (<h1>nav1</h1>) }} />
-                                <Route path='/student/nav2' render={() => { return (<h1>nav2</h1>) }} />
-                                <Route path='/student' render={() => { return (<h1>Home</h1>) }} />
-
-                            </Switch>
+                    </div>
+                </div>
 
 
-                        </div>
 
-                    </Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}></Footer>
-      </Layout>
-    );
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '40px 0' }}>
+                    </Breadcrumb>
+                    <Layout className="Student-page-background" style={{ height: '100%' }}>
+                        <Content style={{ margin: '24px 16px 0' }}>
+                            <div className="Student-content-background" style={{ padding: 24, height: '100%' }}>
+                                <Switch>
 
-  }
+                                    <Route path='/student/Home' render={() => { return (<h1>Home</h1>) }} />
+                                    <Route path='/student/nav1' render={() => { return (<h1>nav1</h1>) }} />
+                                    <Route path='/student/nav2' render={() => { return (<h1>nav2</h1>) }} />
+                                    <Route path='/student' render={() => { return (<h1>Home</h1>) }} />
+
+                                </Switch>
+
+
+                            </div>
+
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}></Footer>
+            </Layout>
+        );
+
+    }
 
 
 }
