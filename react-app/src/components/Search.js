fc2b774422debb0bc85aa499a479410e34e3eca8
Jason McKearney <jason.mckearney@snhu.edu>
2021-10-31 11:55:03 -0400

    Search Functionality works

--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -1,175 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox, Carousel } from 'antd';
 import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './Search.css';
-import Cookies from 'js-cookie';
+//import Cookies from 'js-cookie';
 import axios from 'axios'
+import { tsParameterProperty } from '@babel/types';
+import { useParams } from 'react-router';
 
 export default class search extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            sUsernameToSearch : '',
-            //usernamesFoundArray: []
-            StudentData: []
+            searchText : '',
+            searchResults: ''
         }
-       this.sUsernameToSearch = this.sUsernameToSearch.bind(this);
+        this.getResults = this.getResults.bind(this);
+        this.searchText = this.searchText.bind(this);
     }
 
-    sUsernameToSearch(event) {
-        this.setState({ sUsernameToSearch: event.target.value })
+    searchText(event) {
+        this.setState({ searchText: event.target.value })
     }
-
- /*   usernamesFoundArray(event) {
-        this.setState({usernamesFoundArray: event.target.value})
-    }
-*/
-   // const [data, setData] = useState([]);
-   
-   
-   LookForStudents(event) {
-        fetch('http://localhost:16648/api/Admin/FindStudents', {
-            headers: {
+    getResults(){
+        let currentComponent = this;
+        var test = this.state.searchText;
+        // Passing parameter to Web API through address
+        fetch('http://localhost:16648/api/Admin/FindStudents/'+this.state.searchText, {
+            mode: 'cors', // this cannot be 'no-cors'
+            headers: {                
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             },
-            method: 'GET',
-            body: JSON.stringify({
-                sUsernameToSearch: this.state.sUsernameToSearch
-            })
-        }).then((Response) => Response.json())
-        .then((result) => {
-            Cookies.set('test', "test")
-            this.setState({
-                StudentData: result.data.username
-            })
-            
-            
-        })
-    }
-
-
-    /*LookForStudents() {
-        axios('http://localhost:16648/api/Admin/FindStudents'), {
-              params:{
-                  sUsernameToSearch: this.state.sUsernameToSearch.toString()
-              }
+            method: 'POST',
+        }).then(res=>res.clone().json())
+        .then(function(res) {
+            console.log("hello " + res[0].username)
+            var loopData = ''
+            var i;
+            for (i = 0; i < res.length; i++)
+            {
+                console.log("Next User: " + res[i].username)
+                loopData += `<li>${res[i].username}</li>`
             }
-            .then(function (response) {
-                this.setState({
-                    StudentData: response.data
-                })
-            })
-        }
-*/
-
-
-
-
-
-
-        /*  headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            method: 'GET',
-          body: JSON.stringify({
-                sUsernameToSearch: this.state.sUsernameToSearch.toString()
-            })
-         // }).then((Response) => Response.json())
-          //.then((result) => {
-              .then(result => {
-                this.setState({
-                    StudentData: result.data
-                });
-            })
-    }
-*/          
-/*        
-        // Admin find students that match ...
-        axios.get('http://localhost:16648/api/Admin/FindStudents', {
-          /*  headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            
-            method: 'GET',
-            body: JSON.stringify({
-                sUsernameToSearch: this.state.sUsernameToSearch
-            })
-        }).then((data) => data.json())
-            .then(data => {
-             // create an array of contacts only with relevant data
-            const newContacts = response.data.map(c => {
-            return {
-              name: c.name
-              
-            };
-          });
-  
-          // create a new "State" object without mutating 
-          // the original State object. 
-          const newState = Object.assign({}, this.state, {
-            contacts: newContacts
-          });
-
-           // store the new state object in the component's state
-            this.setState(newState);
+            currentComponent.setState({searchResults: loopData})
         })
-        .catch(error => console.log(error));
-*/
+    }
+    
     render() {
-        console.log(this.state.usernamesFoundArray);
+        const {searchResults} = this.state
+        
         return (
             <div className="container-search">
                 <div >
                     <h1>Search</h1>
                     <div>
-                        <form>
-                            <div >
-
-                                <div id = "wrapper">
-                                    <div >
-                                     <input id = "searchBar" Placeholder="Please input a Students name or ID" onChange={this.sUsernameToSearch} />
-                                     </div>
-                                   
-                                    <div >
-                                        <button onClick={this.LookForStudents} id = "primary-button" ClassName="primary-button" htmlType="submit">Search </button>
-                                    </div>
-                                </div>
-
-                            </div>
-
-
-                        </form>
-
+                        <Form>
+                            <Form.Item>
+                                <Input type="text" onChange={this.searchText} id = "searchBar" Placeholder="Please input a Students name or ID" />
+                            </Form.Item>
+                            <button onClick={this.getResults} id = "primary-button" htmlType="submit">Search </button>    
+                        </Form>
                     </div>
-
-
-
                 </div>
 
                 <section>  
-                <h1>Products List</h1>  
-                <div>  
-                    <div>   
-                        <h1>  
-                            {  
-                                this.state.StudentData.map((p, index) => {  
-                                  return <tr key={index}><td>{p.username}</td></tr>;  
-                                })   
-                            }  
-                        </h1>  
+                    <div>     
+                        <div className="resultsBox"> 
+                            <ul>
+                                <Link className= "student-name" to={'/StudentProfile'}
+                                    dangerouslySetInnerHTML={{__html: searchResults}}>    
+                                </Link>   
+                            </ul>    
+                        </div>  
                     </div>  
-                </div>  
-  
-  
-            </section> 
-
-
-
-                
+                </section> 
             </div>
         );
     } 
