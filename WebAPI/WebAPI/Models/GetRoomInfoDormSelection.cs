f7a38f361b57c04b1c2feac81f2351b098b92689
Jason McKearney <jason.mckearney@snhu.edu>
2022-01-30 17:37:42 -0500

    add untracked files

---
+++ WebAPI/WebAPI/Models/GetRoomInfoDormSelection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+	public class GetRoomInfoDormSelection
+	{
+		public string dorm_id { get; set; }
+
+		public int floorNumber { get; set; }
+		public int numRoommates { get; set; }
+	}
+}
