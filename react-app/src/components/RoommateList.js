42692e7fbb1f35034a788254309de62c3cc9bfb4
Jason McKearney <jason.mckearney@snhu.edu>
2022-02-05 15:19:14 -0500

    Fixed conflicts from Jason-backend to master branches

---
+++ react-app/src/components/RoommateList.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Button, Form, Modal, Input, Card } from 'antd';
+import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import './Student.css';
+import Cookies from 'js-cookie';
+import DormSelection from './DormSelection'
+import StudentHome from './StudentHome'
+import RoommateSelection from './RoommateSelection'
+import StudentInfo from './StudentInfo'
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+//TODO import "./StudentHome.css"
+import defaultlogo from '../img/default_logo.png'
+const { Meta } = Card;
+
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+
+
+export default class RoommateList extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        studentID: 0,
+        studentName:'',
+        studentlist: [],
+        studentObj: {username:'', studentID:'', firstname:'', lastname:''}
+    }
+}
+    getResults(){
+        let currentComponent = this;
+        let studentListLength = this.state.studentlist;
+
+        console.log("Length: " + studentListLength.length)
+
+        // Does not allow for multiple strings to be displayed if the input has not changed by the user
+        if(studentListLength == 0)
+        {
+            // Passing parameter to Web API through address
+            fetch('http://localhost:16648/api/Student/GetDormOccupants?roomID=2', {
+                mode: 'cors', // this cannot be 'no-cors'
+                headers: {                
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                method: 'POST',
+            }).then(res=>res.clone().json())
+            .then(function(res) {
+                var loopData = ''
+                var i;
+                for (i = 0; i < res.length; i++)
+                {
+                   console.log("Next User: " + res[i].username)
+                    console.log("Next studentID: " + res[i].studentID)
+                    if(res[i].username != "")
+                    {
+                        currentComponent.setState({studentName: res[i].username})
+                        currentComponent.setState({studentID: res[i].studentID})
+                        currentComponent.setState({firstname: res[i].firstName})
+                        currentComponent.setState({lastname: res[i].lastName})
+                        
+                        // Add student to list
+                        currentComponent.addItem();
+                    }
+                    // Entries with characters entered do not match any usernames in the database
+                    else
+                        alert("No entries match the character/characters entered.")
+                }
+                currentComponent.setState({searchResults: loopData})
+            })
+        }
+    }
+
+    addItem() {
+
+        const newstudentObj = { username: this.state.studentName, studentID: this.state.studentID, firstname: this.state.firstname, lastname: this.state.lastname }
+      
+        //this.setState({studentObj: newstudentObj} )
+
+        console.log("New-Object " + newstudentObj.username)
+       
+        let newStudentlist = this.state.studentlist;
+
+       for (var i = 0; i < newStudentlist.length; i++)
+       {
+          console.log('New Student List');
+           console.log('-----------------------------');
+           console.log(newStudentlist[i].username);
+           console.log('-----------------------------\n');
+       }
+        console.log( newStudentlist.push(newstudentObj))
+       
+        for (var i = 0; i < this.state.studentlist.length; i++)
+        {
+            console.log('The State List');
+            console.log('-----------------------------');
+            console.log(this.state.studentlist[i].username);
+            console.log('-----------------------------');
+        }
+
+           
+         this.setState({
+             studentlist: newStudentlist
+          });
+          
+
+      }
+
+    state = {
+        showModal: false
+    }
+    
+    listItems() {
+        let studentlist = this.state.studentlist;
+       
+        return (
+            <div>
+            {
+              studentlist.map((val, index) => {
+                return (
+                
+                    <Card className="Roommate1"
+                    hoverable
+                    cover={<img alt="example" src={defaultlogo} />}
+                >   
+                    <p>{val.firstname} {val.lastname}</p>
+                    <p>{val.username}</p>
+                    <p>{val.studentID}</p>
+                    <p><Button danger onClick={() => {
+                          }} type="primary" htmlType="Delete">
+                              Delete
+                          </Button></p>
+                </Card>
+                );
+              })
+            }   
+            </div>    
+        );
+        
+    }
+
+    componentDidMount(){
+        this.getResults();
+    }
+
+  render() {
+    return (
+      <div className="StudentHomeBox-right">
+          <div className="resultsBox">
+                            { this.listItems() }
+                        </div>
+                </div>
+    );
+  }
+}
---
+++ react-app/src/components/RoommateSelection.js
@@ -0,0 +1,212 @@
+import React, { Component } from 'react'
+import "./RoommateSelection.css"
+import Cookies from  'js-cookie'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+
+export default class RoommateSelection extends Component {
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            userSearchString : '',
+            results : '',
+            resultsList : [],
+            studentList : [],
+            user_id: '',
+            firstName: '',
+            lastName: '',
+            year:'',
+            
+
+        }
+
+            this.setSearchText = this.setSearchText.bind(this);
+            this.submitSearchText = this.submitSearchText.bind(this);
+            this.printResults = this.printResults.bind(this);
+            this.addItemResults = this.addItemResults.bind(this);
+            this.setRoommateChoice = this.setRoommateChoice.bind(this);
+            
+    }
+
+
+    
+    setSearchText(event)
+    {
+         this.setState({ userSearchString: event.target.value });
+         console.log(this.state.userSearchString)
+    }
+    addItemResults()
+    {
+
+        const newstudentObj = {user_id: this.state.user_id,firstName: this.state.firstName, lastName: this.state.lastName, year: this.state.year}
+        let newStudentlist = this.state.studentList;
+        newStudentlist.push(newstudentObj)
+
+        this.setState({
+            studentList: newStudentlist
+        })
+
+         console.log("item added to the list")
+    }
+    printResults(){
+
+        
+        console.log(this.state.studentList);
+        return (
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>year</td>
+                        <td></td>
+                    </tr>
+                </thead>
+           
+            <tbody>
+              {
+                this.state.studentList.map((val, index) => {
+                    return (
+                   
+                          <tr>  
+                              
+                              
+                                  <td className="result-word" key={index}> {val.firstName}</td>
+                                  <td className="result-word" key={index}> {val.lastName}</td>
+                                  <td className="result-word" key={index}> {val.year}</td>
+                                  <td><button  className="add-icon"><FontAwesomeIcon onClick = {() => {this.AddStudent(val.user_id)}} type ="submit" icon={faUserPlus} size = "3x" color="green" /> </button></td> 
+                                  
+                              
+                              
+                           
+                              
+                          </tr>
+                          
+                  
+                          
+                  );
+                })
+              }
+            </tbody>
+            </table>
+          );
+        
+    }
+
+    submitSearchText()
+
+    {
+
+        let currentComponent = this;
+
+        console.log(this.state.userSearchString)
+
+        fetch('http://localhost:16648/api/Student/FindRoommateInfo/' + this.state.userSearchString, {
+
+            mode: 'cors', // this cannot be 'no-cors'
+
+            headers: {                
+
+                'Content-Type': 'application/json',
+
+                'Accept': 'application/json',
+
+            },
+
+            method: 'POST',
+
+        }).then(res=>res.clone().json())
+
+        .then(function(res) {
+
+           
+
+           const studentArray = currentComponent.state.studentList.slice();
+
+           console.log("studentArray before filling: " + studentArray)
+
+           var i;
+            for( i = 0; i < res.length; i++)
+
+             {
+
+                 console.log("firstname: " + res[0].firstName)
+
+                 console.log("lastname: " + res[0].lastName)
+
+                 console.log("year: " + res[0].year)
+                 currentComponent.setState({user_id: res[i].user_id})
+                 currentComponent.setState({firstName: res[i].firstName})
+                 currentComponent.setState({lastName: res[i].lastName})
+                 currentComponent.setState({year: res[i].year})
+
+                 currentComponent.addItemResults();
+            }
+
+        });
+
+    }
+    AddStudent(user_id){
+
+       
+        console.log(user_id)
+        console.log("funciton reached")
+        fetch('http://localhost:16648/api/Student/AddRoommate', {
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify({
+
+                uid: Cookies.get("ID"),
+                reciever_id: user_id
+            })
+        }).then((Response) => Response.json())
+        .then((result) => {
+            console.log("response: " + result.status)
+            alert(result.message);
+
+        })
+
+    }
+
+    setRoommateChoice(event)
+    {
+        console.log("test");
+        this.setState({user_id: event})
+       
+
+    }
+    render() {
+        return (
+
+        <div>
+            <div clasName="roommate-container">
+                <div className="wrapper">
+                    <form>
+                        
+                        <input className = "container-object search-bar" onChange= {this.setSearchText} type="text" placeholder="Please enter a name you wish to search"/>
+
+
+                    </form>
+                        <button className ="container-object" id = "primary-button"type ="submit" onClick ={this.submitSearchText}>search</button>
+
+                        </div>
+                
+                
+                        
+                            <div className="container-results">
+                                { this.printResults() } 
+                            </div>
+                        
+            </div>
+         </div>
+            
+            );
+    }
+}
