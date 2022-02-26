4287ea1110b4185f1ac20e3115115a074b9de6c9
NicholasGaston <59942769+NicholasGaston@users.noreply.github.com>
2022-02-26 15:10:18 -0500

    class for question response

---
+++ WebAPI/WebAPI/Models/SurveyQuestions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class SurveyQuestions
+    {
+        public string Question1 { get; set; }
+        public string Question2 { get; set; }
+        public string Question3 { get; set; }
+        public string Question4 { get; set; }
+        public string Question5 { get; set; }
+        public string Question6 { get; set; }
+        public string Question7 { get; set; }
+        public string Question8 { get; set; }
+        public string Question9 { get; set; }
+        public string Question10 { get; set; }
+        public string Question11 { get; set; }  
+        public string Question12 { get; set; }
+    }
+}
