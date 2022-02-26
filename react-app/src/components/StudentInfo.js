cf488ad46938e49457e14fea1a4816288e6cb7a3
NicholasGaston <59942769+NicholasGaston@users.noreply.github.com>
2022-02-26 14:46:39 -0500

    submit button for user

--- react-app/src/components/StudentInfo.js
+++ react-app/src/components/StudentInfo.js
@@ -15,29 +15,6 @@ export default class studentinfo extends Component {
     constructor() {
         super();
         
-        
-            /*fetch('http://localhost:16648/api/Student/', {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                method: 'POST',
-                body: JSON.stringify({
-                    username: Cookies.get("username")
-                })
-            }).then((Response) => Response.json())
-                .then((result) => {
-                    var ID = result.studentID;
-                    var firstName = result.firstName;
-                    var lastName = result.lastName;
-                    var year = result.year;
-
-                    Cookies.set("ID", ID);
-                    Cookies.set("FN", firstName);
-                    Cookies.set("LN", lastName);
-                    Cookies.set("YR", year);
-                })
-*/
 
         this.state = {
             
@@ -52,15 +29,74 @@ export default class studentinfo extends Component {
                 { label: "Work Study", value: 6 },
                 { label: "Other", value: 7 }
               ],
-            userAnswers:[]
+            userAnswers:{Question1:'placeholder', Question2:'', Question3:'', Question4:'',Question5:'', Question6:'', 
+                        Question7:'',Question8:'', Question9:'', Question10:'', Question11:'', Question12:''}
         }
        
-       
+       //this.userAnswers = this.userAnswers.bind(this);
        this.onChangeCheckbox = this.onChangeCheckbox.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
+    onChangeQuestion(event,questionNum){
 
+        let newUserAnswers = this.state.userAnswers;
+
+        console.log("Question number" + questionNum);
+        console.log("Question Value" + event);
+        if(questionNum == 1)
+        {
+            
+            newUserAnswers.Question1 = event;
+            this.setState({userAnswers: newUserAnswers});
+            console.log("State value for Question 1: " + this.state.userAnswers.Question1)
+        }
+        else if(questionNum == 2)
+        {
+            newUserAnswers.Question2 = event;
+        }
+        else if(questionNum == 3)
+        {
+            newUserAnswers.Question3 = event;
+        }
+        else if(questionNum == 4)
+        {
+            newUserAnswers.Question4 = event;
+        }
+        else if(questionNum == 5)
+        {
+            newUserAnswers.Question5 = event;
+        }
+        else if(questionNum == 6)
+        {
+            newUserAnswers.Question6 = event;
+        }
+        else if(questionNum == 7)
+        {
+            newUserAnswers.Question7 = event;
+        }
+        else if(questionNum == 8)
+        {
+            newUserAnswers.Question8 = event;
+        }
+        else if(questionNum == 9)
+        {
+            newUserAnswers.Question9 = event;
+        }
+        else if(questionNum == 10)
+        {
+            newUserAnswers.Question10 = event;
+        }
+        else if(questionNum == 11)
+        {
+            newUserAnswers.Question11 = event;
+        }
+        else if(questionNum == 12)
+        {
+            newUserAnswers.Question12 = event;
+        }
+
+    }
 
     onChangeCheckbox = e => {
         const isChecked = !this.state.checked;
@@ -91,17 +127,17 @@ export default class studentinfo extends Component {
                 <form> 
                     <div className="question-wrapper">
                         <label className ="Form-Label">When do you wake up? </label>
-                        <input type ="time"></input>
+                        <input onChange={e => this.onChangeQuestion(e.target.value, 1)}type ="time"></input>
                     </div>
 
                     <div className="question-wrapper" >
                         <label className ="Form-Label">What time do you sleep?</label>
-                        <input type = "time"></input>
+                        <input onChange={e => this.onChangeQuestion(e.target.value, 3)} type = "time"></input>
                     </div>
                   
                     <div className="question-wrapper">
                         <label className ="Form-Label"> What best decribes your study habits?</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 3)} className='form-select'>
                             <option>Please selet an option</option>
                             <option>Ambient noise: I dont mind some background noise but I need to have fewer distractions to study effectively.</option>
                             <option>Don't take work home: I do most of my schoolwork at the library or elsewhere out of my room. I try not to so a lot of work at home.</option>
@@ -112,7 +148,7 @@ export default class studentinfo extends Component {
 
                     <div className="question-wrapper">
                     <label className ="Form-Label">When do you study best?</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 4)} className='form-select'>
                             <option>Please select an option</option>
                             <option>Can do homework whenever</option>
                             <option>After 8pm</option>
@@ -124,7 +160,7 @@ export default class studentinfo extends Component {
                    
                     <div className="question-wrapper">
                     <label className ="Form-Label">Which describes how you prefer to use your room?</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 5)} className='form-select'>
                             <option>Please select an option</option>
                             <option>Room is a sanctuary: I perfer privacy and personal space.</option>
                             <option>Room is a social hub: I enjoy people dropping by often.</option>
@@ -134,7 +170,7 @@ export default class studentinfo extends Component {
 
                     <div className="question-wrapper">
                         <label className ="Form-Label">Which best describes your bedroom's apperance?</label>
-                        <select className='form-select'> 
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 6)} className='form-select'> 
                             <option>Please select an option</option>
                             <option>Casual: My personal space is farily clean with some clutter but I value keeping common areas pretty clean.</option>
                             <option>Messy: I sometimes drop my stuff right where I am stadning and it stays there until I need it again. Cleanliness is not a high priority</option>
@@ -143,7 +179,7 @@ export default class studentinfo extends Component {
                     </div>
                     <div className="question-wrapper" >
                     <label className ="Form-Label">What are your preferences about lending your items (clothes, food, electronics, etc.)?</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 7)} className='form-select'>
                             <option>Please select an option</option>
                             <option>Not lending out my items</option>
                             <option>Let anyone borrow my items</option>
@@ -153,7 +189,7 @@ export default class studentinfo extends Component {
 
                     <div className="question-wrapper">
                     <label className ="Form-Label">What are your preferences on your guests staying in the room?</label>
-                    <select className='form-select' >
+                    <select onChange={e => this.onChangeQuestion(e.target.value, 8)} className='form-select' >
                         <option>Will allow anyone to come by and stay with us when discussed between roommate</option>
                         <option>Will not allow any vistors overnight to stay but people can drop by.</option>
                         <option>Will allow any vistors to drop by or stay overnight.</option>
@@ -168,12 +204,13 @@ export default class studentinfo extends Component {
                             options = {this.state.options}
                             value={this.state.values}
                             className='form-select'
+                           
                             />
                           
                      </div>
                      <div className="question-wrapper">
                         <label className ="Form-Label">What do you do on a typical weekend?</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 10)} className='form-select'>
                             <option>Please select an option</option>
                             <option>On campus most weekends</option>
                             <option>Go home most weekends</option>
@@ -185,7 +222,7 @@ export default class studentinfo extends Component {
 
                     <div className="question-wrapper">
                         <label className ="Form-Label">Describe your smoking/vaping habits</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 11)} className='form-select'>
                             <option>Please select an option</option>
                             <option>Never:this means you literally never smoke</option>
                             <option>Occasionally: while you do not typically smoke you may have a cigarette once in a while.</option>
@@ -195,7 +232,7 @@ export default class studentinfo extends Component {
 
                     <div className="question-wrapper">
                         <label className ="Form-Label">How do you describe your alcohol use? This will not impact your conduct record.</label>
-                        <select className='form-select'>
+                        <select onChange={e => this.onChangeQuestion(e.target.value, 12)} className='form-select'>
                             <option>Please select an option</option>
                             <option>Not a drinker and will not allow it in the unit</option>
                             <option>Not a drinker but may allow in the unit</option>
