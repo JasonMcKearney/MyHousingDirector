52e046638176035379e9526d9b22a534ec19f5b0
Jason McKearney <jason.mckearney@snhu.edu>
2022-03-28 19:57:29 -0400

    AdminDormRequest page GUI created

--- react-app/src/components/AdminDormRequests.js
+++ react-app/src/components/AdminDormRequests.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCoffee, faSubway } from "@fortawesome/free-solid-svg-icons";
 import "./AdminDormRequests.css";
 
 // Export means any module (AdminDashboard.js file in this case) can use this script by importing it
@@ -15,14 +15,18 @@ export class AdminDormRequests extends Component {
             <table>
                 <thead>
                     <tr>
-                        <td className ="table-head">First Name</td>
-                        <td className ="table-head">Last Name</td>
-                        <td className ="table-head">year</td>
-                        <td></td>
+                        <td className ="table-head">Full-name</td>
+                        <td className ="table-head">BuildingName</td>
+                        <td className ="table-head">Floor Number</td>
+                        <td className ="table-head">Room Number</td>
+                        <td className ="table-head">Accept</td>
+                        <td className ="table-head">Decline</td>
+
+                       
                     </tr>
                 </thead>
-                <tbody>
-                    {this.state.studentList.map((val, index) => {
+              {/*  <tbody>
+              https://fontawesome.com/icons/check?s=solid   for button
                         return (
                             <tr>
                                 <td className="result-word" key={index}>
@@ -54,18 +58,39 @@ export class AdminDormRequests extends Component {
                         );
                     })}
                 </tbody>
+            */}
             </table>
         );
     }
     render(){
         return(
             <div class="container">
-                <div className="container-results">{this.printResults()}</div>
-                <label style={{fontSize:'180%', paddingBottom: '3%', paddingRight:'8%'}}>Dorm Requests Page</label>
-                <div class="requests-box">
+                <div style={{borderRadius: '10px', borderWidth: '5px'}}>
+                    <label style={{fontSize:'180%', paddingBottom: '1%', paddingRight:'0%', fontSize: '200%'}}><strong>Dorm Requests Page</strong></label>
+                </div>
+                <div className="container-results" style={{width:'80%'}}>{this.printResults()}</div>
+             {/*   <div class="requests-box">
                     <div style={{padding:'10%', paddingTop:'14%'}}>test</div>
                 </div>
-            </div>    
-        );
-    }
+        */}
+            <div class="container-BuildingDataBox">
+                <label style={{paddingTop:'3%'}}>Available Dorm Rooms</label>
+                <div class="buildingsList-div">
+                    <li>
+                        building name
+                        <ul>
+                            studentNames
+                        {/* {availableBuildingsList.map((data) => (
+                            <li key={data.name}>
+                                {data.name} ({data.numRoomsAvailable})                
+                            </li>
+                        ))}
+                        */}
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </div>    
+    );
+  }
 }
