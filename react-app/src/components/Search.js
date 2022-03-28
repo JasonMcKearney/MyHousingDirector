1dc9fe25b76f86a0a084eb2ca3f2e9e3ed2caccf
jackwulanbatu <60981213+jackwulanbatu@users.noreply.github.com>
2022-03-28 11:28:07 +0800

    search updates

--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -1,188 +1,178 @@
-import React, { Component } from 'react'
-import {Table} from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import 'antd/dist/antd.css';
-import './Search.css';
-//import Cookies from 'js-cookie';
-import axios from 'axios'
-import { tsParameterProperty } from '@babel/types';
-import { useParams } from 'react-router';
-import Cookies from 'js-cookie';
+import React, { Component } from "react";
+import { Input, Table, Button, Modal, Descriptions } from "antd";
+import "antd/dist/antd.css";
+import "./Search.css";
+import Cookies from "js-cookie";
 
-
-
-// Referenced https://www.cubui.com/blog/react/render-arrays-react-js/ for help using a list
-
+const { Search } = Input;
 export default class search extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            searchText : '',
-            searchResults: '',
-            user_id: 0,
-            studentName:'',
-            studentlist: [],
-            studentObj: {username:'', user_id:''},
-        }
+    this.state = {
+      searchText: "",
+      searchResults: "",
+      user_id: 0,
+      studentName: "",
+      studentlist: [],
+      studentObj: { username: "", user_id: "" },
+    };
 
-     
-        //Student Object to store all the student data
-        
-        
-    
-        // Gives access to functions below... 
-        this.getResults = this.getResults.bind(this);
-        this.searchText = this.searchText.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.listItems = this.listItems.bind(this);      
-       
+    //Student Object to store all the student data
+
+    // Gives access to functions below...
+    this.getResults = this.getResults.bind(this);
+    this.searchText = this.searchText.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.listItems = this.listItems.bind(this);
+  }
+
+  searchText(event) {
+    this.setState({ searchText: event.target.value });
+    // this.setState({ studentlist: [] })
+  }
+
+  // Is called after studentName is set, adds the student to the list
+  addItem() {
+    const newstudentObj = {
+      username: this.state.studentName,
+      user_id: this.state.user_id,
+    };
+
+    //this.setState({studentObj: newstudentObj} )
+
+    console.log("New-Object " + newstudentObj.username);
+
+    let newStudentlist = this.state.studentlist;
+
+    for (var i = 0; i < newStudentlist.length; i++) {
+      console.log("New Student List");
+      console.log("-----------------------------");
+      console.log(newStudentlist[i].username);
+      console.log("-----------------------------\n");
+    }
+    console.log(newStudentlist.push(newstudentObj));
+
+    for (var i = 0; i < this.state.studentlist.length; i++) {
+      console.log("The State List");
+      console.log("-----------------------------");
+      console.log(this.state.studentlist[i].username);
+      console.log("-----------------------------");
     }
 
-    searchText(event) {
-        this.setState({ searchText: event.target.value });
-       // this.setState({ studentlist: [] })
-    }
+    this.setState({
+        studentlist: JSON.parse(JSON.stringify(newStudentlist)),
+    });
+  }
 
-    // Is called after studentName is set, adds the student to the list
-    addItem() {
+  // Returns list of students in a list format and updates cookies for later use throughout the application
+  listItems() {
+    let studentlist = this.state.studentlist;
+    const columns = [
+      {
+        title: "ID",
+        dataIndex: "user_id",
+        //   render: (text) => <a>{text}</a>,
+      },
+      {
+        title: "Username",
+        dataIndex: "username",
+        //   render: (text) => <a>{text}</a>,
+      },
+      {
+        title: "Action",
+        render: (text, record) => (
+          <Button
+            size="small"
+            onClick={() => {
+              this.props.history.push("/home/StudentProfile");
+              Cookies.set("student", record.username);
+            }}
+          >
+            view
+          </Button>
+        ),
+      },
+      ];
+     console.log(12233, studentlist)
+    return (
+        <Table columns={columns} dataSource={studentlist} pagination={false} />
+    //   <Table
+    //     columns={columns}
+    //     dataSource={[
+    //       { username: "jkbhjvghcfgcghhjknklj", user_id: "user_idjhgf" },
+    //     ]}
+    //     pagination={false}
+    //   />
+    );
+  }
 
-        const newstudentObj = { username: this.state.studentName, user_id: this.state.user_id}
-      
-        //this.setState({studentObj: newstudentObj} )
+  // Find results if there are any in the database
+  getResults() {
+    let currentComponent = this;
+    let studentListLength = this.state.studentlist;
 
-        console.log("New-Object " + newstudentObj.username)
-       
-        let newStudentlist = this.state.studentlist;
+    console.log("Length: " + studentListLength.length);
 
-       for (var i = 0; i < newStudentlist.length; i++)
-       {
-          console.log('New Student List');
-           console.log('-----------------------------');
-           console.log(newStudentlist[i].username);
-           console.log('-----------------------------\n');
-       }
-        console.log( newStudentlist.push(newstudentObj))
-       
-        for (var i = 0; i < this.state.studentlist.length; i++)
+    // Does not allow for multiple strings to be displayed if the input has not changed by the user
+    if (studentListLength == 0) {
+      // Passing parameter to Web API through address
+      fetch(
+        "http://localhost:16648/api/Admin/FindStudents/" +
+          this.state.searchText,
         {
-            console.log('The State List');
-            console.log('-----------------------------');
-            console.log(this.state.studentlist[i].username);
-            console.log('-----------------------------');
+          mode: "cors", // this cannot be 'no-cors'
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          method: "POST",
         }
+      )
+        .then((res) => res.clone().json())
+        .then(function (res) {
+          console.log("hello " + res[0].username);
+          var loopData = "";
+          var i;
+          for (i = 0; i < res.length; i++) {
+            // console.log("Next User: " + res[i].username)
+            console.log("Next User_id: " + res[i].user_id);
+            if (res[i].username != "") {
+              currentComponent.setState({ studentName: res[i].username });
+              currentComponent.setState({ user_id: res[i].user_id });
 
-           
-         this.setState({
-             studentlist: newStudentlist
-          });
-          
-
-      }
-
-      // Returns list of students in a list format and updates cookies for later use throughout the application
-      listItems() {
-        let studentlist = this.state.studentlist;
-       
-        return (
-          
-            <table>
-                    <thead>
-                        <tr>
-                            <th className="search-Head">ID</th>
-                            <th className="search-Head">Username</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-            <tbody>
-            {
-              studentlist.map((val, index) => {
-                return (
-                
-                 <tr className='result-node' >
-                      
-                      <td className='student-info'> {val.user_id}</td>
-                      <td className='student-info'> {val.username}</td>
-                      <td className='result-button'><button onClick={() => { this.props.history.push('/home/StudentProfile'); Cookies.set("student", val.username); } } id ='primary-button' >View</button></td>
-                 </tr>
-                );
-              })
+              // Add student to list
+              currentComponent.addItem();
             }
-            </tbody>
-                            
-        </table>
-        
-        );
-      }
-
-    // Find results if there are any in the database
-    getResults(){
-        let currentComponent = this;
-        let studentListLength = this.state.studentlist;
-
-        console.log("Length: " + studentListLength.length)
-
-        // Does not allow for multiple strings to be displayed if the input has not changed by the user
-        if(studentListLength == 0)
-        {
-            // Passing parameter to Web API through address
-            fetch('http://localhost:16648/api/Admin/FindStudents/'+this.state.searchText, {
-                mode: 'cors', // this cannot be 'no-cors'
-                headers: {                
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                method: 'POST',
-            }).then(res=>res.clone().json())
-            .then(function(res) {
-               console.log("hello " + res[0].username)
-                var loopData = ''
-                var i;
-                for (i = 0; i < res.length; i++)
-                {
-                   // console.log("Next User: " + res[i].username)
-                    console.log("Next User_id: " + res[i].user_id)
-                    if(res[i].username != "")
-                    {
-                        currentComponent.setState({studentName: res[i].username})
-                        currentComponent.setState({user_id: res[i].user_id})
-                        
-                        // Add student to list
-                        currentComponent.addItem();
-                    }
-                    // Entries with characters entered do not match any usernames in the database
-                    else
-                        alert("No entries match the character/characters entered.")
-                }
-                currentComponent.setState({searchResults: loopData})
-            })
-        }
+            // Entries with characters entered do not match any usernames in the database
+            else alert("No entries match the character/characters entered.");
+          }
+          currentComponent.setState({ searchResults: loopData });
+        });
     }
-    
-    render() {
-        return (
-            <div className="container-search">
+  }
 
-            <div >
-                <h1>Search</h1>
-                <div>
-                    <form>
-                        <input type="text" onChange ={this.searchText} id = "searchBar" Placeholder="Please input a Students name or ID" />
-
-                    </form>
-                    <button onClick={this.getResults} id = "primary-button" htmlType="submit">Search </button>    
-
-                </div>
-
-            </div>
-                <section>  
-                    <div>    
-                        <div className="resultsBox">
-                            { this.listItems() }
-                        </div>
-                    </div>  
-                </section>
+  render() {
+    return (
+      <div className="container-search">
+        <div>
+          <h1>Search</h1>
+          <Search
+            onChange={this.searchText}
+            placeholder="Please input a Students name or ID"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={this.getResults}
+            style={{ margin: "0 0 30px 0 " }}
+          />
         </div>
-        );
-    } 
+        <section>
+          <div>
+            <div className="resultsBox">{this.listItems()}</div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
---
+++ react-app/src/components/mockData.js
@@ -0,0 +1,20 @@
+export const mockDataSource = [
+  {
+    key: "1",
+    Username: "Jack1",
+    ID: 32,
+    Building: "A",
+    Dorm: "Dorm1",
+    Floor: 3,
+    roommateNum: 31,
+  },
+  {
+    key: "2",
+    Username: "Jack2",
+    ID: 42,
+    Building: "B",
+    Floor: 4,
+    Dorm: "Dorm2",
+    roommateNum: 35,
+  },
+];
